504192fbce21c08f2579d5802490c28101729389
old-story <202438995@qq.com>
2021-06-25 10:14:59 +0000

    feat(main.cpp): add ()

--- main.cpp
+++ main.cpp
@@ -9,34 +9,40 @@ bool _isdigit(int c) {
 	return c >= '0' && c <= '9';
 }
 
-Express* getExpress(std::stringstream &ss, Express *root = nullptr) {
+Express* getExpress(std::stringstream &ss, Express *root = nullptr, int parentheses = 0) {
 
-	std::string value, op;
+	std::string _parentheses, value, op;
 	if (root == nullptr) {
 		ss >> value;
-		root = new ValueExp(std::stoi(value));
+		if (value[0] == '(') {
+			root = getExpress(ss, root, parentheses + 1);
+		} else 
+			root = new Value(std::stoi(value));
 	}
 
 	ss >> op;
 
 	switch (op[0]) {
 		case '+': {
-				  auto exp = new AddExp(root, getExpress(ss));
+				  auto exp = new Add(root, getExpress(ss));
 				  return exp;
 			  }
 		case '-': {
-				  auto exp = new SubExp(root, getExpress(ss));
+				  auto exp = new Sub(root, getExpress(ss));
 				  return exp;
 			  }
 		case '*': {
 				  ss >> value;
-				  auto exp = new MulExp(root, new ValueExp(std::stoi(value)));
+				  auto exp = new Mul(root, new Value(std::stoi(value)));
 				  return getExpress(ss, exp);
 			  }
 		case '/': {
 				  ss >> value;
-				  auto exp = new DivExp(root, new ValueExp(std::stoi(value)));
+				  auto exp = new Div(root, new Value(std::stoi(value)));
 				  return getExpress(ss, exp);
+			  }
+		case ')': {
+				  return root;
 			  }
 		default: {
 				 return root;
